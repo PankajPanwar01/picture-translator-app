bf96fe51c144205dc3cfb9e30534e05e85d7ebff
Pankaj Panwar <pankajpanwar2105@gmail.com>
2026-04-25 01:54:31 +0530

    Backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const app = express();
 
 // Middleware
-app.use(cors());
+const cors = require("cors");
+
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
